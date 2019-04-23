95e6b1a914a8e2e4cb36142ce94bafe8daf98270
v8178508 <48015015+v8178508@users.noreply.github.com>
2019-04-23 19:29:12 +0100

    Create faction-recruitment.js

---
+++ faction-recruitment.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Faction Recruitment faction
+// @author       Skipper [390936]
+// @version      0.0.1
+// @description  Removes those already in a faction from advanced seacrch
+// @include      *.torn.com/userlist.php?*
+// ==/UserScript==
+
+function removeMembers() {
+    document.querySelectorAll('.user-info-list-wrap > li').forEach(element => {
+
+        if (element.querySelector('#icon9') || element.querySelector('#icon74')) {
+            element.style.display = 'none';
+        }
+    });
+}
+
+window.addEventListener('load', function () {
+    removeMembers();
+}, false);
+
+var pagination = document.getElementsByClassName('pagination-wrap');
+
+for (let i = 0; i < pagination.length; i++) {
+    pagination[i].addEventListener("click", function () {
+        removeMembers();
+    })
+}
